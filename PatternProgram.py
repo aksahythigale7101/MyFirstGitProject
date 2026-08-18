2bd971224dcabb5e645ff330ee504f586006dd1d
aksahythigale7101 <akshaythigale7101@gmail.com>
2026-08-19 00:12:07 +0530

    Square&TrainglePatternAdd

---
+++ PatternProgram.py
@@ -0,0 +1,18 @@
+def SQUAREPATTERN():
+    for i in range(1, 5):
+        for j in range(1, 5):
+            print("*", end=" ")
+
+        print()
+
+#-----------------------------------
+
+def RIGHTTRAINGLE():
+    for i  in range(1,5):
+        for j in range(i):
+            print("*", end=" ")
+
+        print()
+
+
+# --------------------------------------
